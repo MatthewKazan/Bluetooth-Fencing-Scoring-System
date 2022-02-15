6887efae10497f5b16203c148155a337d157731e
Matt Kazan <Mattbkazan@icloud.com>
2022-02-15 12:47:15 -0500

    change name of get mac file

--- Get_mac/Get_mac.ino
+++ Get_mac/Get_mac.ino
@@ -1,11 +1,11 @@
 #include "WiFi.h"
- 
+//gets the mac address of the current esp32 board
 void setup(){
   Serial.begin(115200);
   WiFi.mode(WIFI_MODE_STA);
   Serial.println(WiFi.macAddress());
 }
- 
+
 void loop(){
 
 }
